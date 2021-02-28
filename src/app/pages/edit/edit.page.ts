9cddc729f068ae5bcf849629749b008a948d02ba
Andrii <andrii.m@upland.me>
2021-02-28 13:40:38 +0200

    update edit profile

--- src/app/pages/edit/edit.page.ts
+++ src/app/pages/edit/edit.page.ts
@@ -1,8 +1,12 @@
 /** */
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '@app/services/data.service';
 import { environment } from '@env/environment';
+import { error } from 'protractor';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-edit',
@@ -17,8 +21,12 @@ export class EditPage implements OnInit {
     { value: 'man', name: 'Mens' },
     { value: 'woman', name: 'Vrouw' },
   ];
+
+  public userDocRef: DocumentReference;
   constructor(
     public serviceProvider: DataService,
+    private firestore: AngularFirestore,
+    private angularFireAuth: AngularFireAuth,
     public formBuilder: FormBuilder
   ) {
     this.data = environment.editInfo;
@@ -34,7 +42,6 @@ export class EditPage implements OnInit {
           Validators.required,
         ]),
       ],
-      // email: ['', Validators.compose([Validators.required, Validators.email])],
       firstName: [
         '',
         Validators.compose([
@@ -56,9 +63,52 @@ export class EditPage implements OnInit {
 
   ngOnInit() {
     this.editPageForm.valueChanges.subscribe((res) => console.log(res));
+    this.getUserData();
   }
 
-  applyFilter = () => {
-    console.log('this.editPageForm.value', this.editPageForm.value);
+  getUser() {
+    return from(this.angularFireAuth.currentUser);
+  }
+
+  public getUserData() {
+    const userId = localStorage.getItem('userId');
+    const userRef = this.firestore.collection('Users').doc(userId);
+    const getData = from(userRef.get()).subscribe(
+      (ref: any) => {
+        this.userDocRef = ref;
+        if (ref.exists) {
+          const userDataDetails = ref.data();
+          console.log('userDataDetails', userDataDetails);
+          if (userDataDetails) {
+            this.editPageForm
+              .get('firstName')
+              .setValue(userDataDetails.firstName);
+            this.editPageForm
+              .get('lastName')
+              .setValue(userDataDetails.lastName);
+            this.editPageForm.get('phone').setValue(userDataDetails.phone);
+            this.editPageForm
+              .get('birthDate')
+              .setValue(userDataDetails.birthDate);
+            this.editPageForm.get('sex').setValue(userDataDetails.sex);
+          }
+        } else {
+          console.log('No such document!');
+        }
+      },
+      (error) => {
+        console.log('error, userRef', error);
+      }
+    );
+  }
+
+  applyForm = () => {
+    const collectinoRef = this.firestore.collection('/Users');
+    console.log('collectinoRef', collectinoRef);
+    return from(
+      collectinoRef
+        .doc(localStorage.getItem('userId'))
+        .set(this.editPageForm.value)
+    );
   };
 }
