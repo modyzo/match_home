a3596f5131c10e7104451689c334852a71f2dec2
Andrii <andrii.m@upland.me>
2021-03-02 21:58:09 +0200

    upload image

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -5,14 +5,17 @@ import { DataService } from '@app/services/data.service';
 import { Router } from '@angular/router';
 import { TinderIconsComponent } from '../tinder-icons/tinder-icons.component';
 import { environment } from '@env/environment';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
-  userDetail: { userDetails: string; }[];
+  userDetail: { userDetails: string }[];
   modalGold: any;
   modalRefreshData: any;
   @ViewChild('Slides', { static: false }) slides: IonSlides;
@@ -21,13 +24,19 @@ export class ContactsComponent implements OnInit {
     direction: 'horizontal',
     autoplay: {
       delay: 2000,
-    }
+    },
   };
   index = 0;
   showButton: boolean;
+  userDataDetails;
 
-  constructor(public dataService: DataService,
-    public route: Router) {
+  public objectUrl;
+  constructor(
+    public dataService: DataService,
+    private firestore: AngularFirestore,
+    private fireStorage: AngularFireStorage,
+    public route: Router
+  ) {
     this.userDetail = environment.details;
     this.modalGold = environment.gold;
     this.modalRefreshData = environment.refresh;
@@ -35,7 +44,37 @@ export class ContactsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getUserData();
   }
+
+  public getUserData() {
+    const userId = localStorage.getItem('userId');
+    const userRef = this.firestore.collection('Users').doc(userId);
+    const getData = from(userRef.get()).subscribe(
+      (ref: any) => {
+        if (ref.exists) {
+          this.userDataDetails = ref.data();
+          console.log('userDataDetails', this.userDataDetails);
+
+          //
+          const pathReference = this.fireStorage.ref(
+            this.userDataDetails.profilePicture
+          );
+
+          from(pathReference.getDownloadURL()).subscribe((dataLink) => {
+            this.objectUrl = dataLink;
+          });
+          //
+        } else {
+          console.log('No such document!');
+        }
+      },
+      (error) => {
+        console.log('error, userRef', error);
+      }
+    );
+  }
+
   openGoldModal() {
     this.dataService.openModal(TinderIconsComponent, this.modalGold);
   }
@@ -50,7 +89,7 @@ export class ContactsComponent implements OnInit {
     }
   }
   async change() {
-    await this.slides.getActiveIndex().then(data => this.index = data);
+    await this.slides.getActiveIndex().then((data) => (this.index = data));
     if (this.index === 0) {
       this.showButton = true;
     } else if (this.index !== 0) {
